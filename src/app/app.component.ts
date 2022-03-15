9855879ade2ad0874b71f414acf48ca537d0fcdf
Ivan Icov <ivan.icov@student.um.si>
2022-03-15 10:37:22 +0100

    Trying out angular functionalities for working with components(interpolation, pipes, ngfor, ngif, creating new component, parameters)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  ngOnInit(): void {
+    this.moviesInTheaters = [{
+      title: 'Spider-Man',
+      releaseDate: new Date(),
+      price: 1400.99
+    },
+    {
+      title: 'Moana',
+      releaseDate: new Date(),
+      price: 300.99
+    }];
+
+    this.moviesFutureReleases = [{
+      title: 'Avengers',
+      releaseDate: new Date(),
+      price: 456.99
+    },
+    {
+      title: 'Toy story',
+      releaseDate: new Date(),
+      price: 789.99
+    }];
+  }
+
+  moviesInTheaters;
+  moviesFutureReleases;
   title = 'angular-movies';
+
+  duplicateNumber(num: number) {
+    return num * 2;
+  }
 }
