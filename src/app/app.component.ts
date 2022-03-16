ad7e48b5a6c1d21b6ac8030532ec0f36328eadef
Ivan Icov <ivan.icov@student.um.si>
2022-03-16 19:50:00 +0100

    Created neccessary components and added routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,53 +5,6 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
 
-  ngOnInit(): void {
-    this.moviesInTheaters = [{
-      title: 'Spider-Man',
-      releaseDate: new Date(),
-      price: 1400.99
-    },
-    {
-      title: 'Moana',
-      releaseDate: new Date(),
-      price: 300.99
-    },
-    {
-      title: 'Moana',
-      releaseDate: new Date(),
-      price: 300.99
-    },
-    {
-      title: 'Moana',
-      releaseDate: new Date(),
-      price: 300.99
-    },
-    {
-      title: 'Moana',
-      releaseDate: new Date(),
-      price: 300.99
-    },
-    {
-      title: 'Moana',
-      releaseDate: new Date(),
-      price: 300.99
-    },
-    {
-      title: 'Moana',
-      releaseDate: new Date(),
-      price: 300.99
-    }];
-
-    this.moviesFutureReleases = [];
-  }
-
-  moviesInTheaters;
-  moviesFutureReleases;
-  title = 'angular-movies';
-
-  handleRating(rate: number) {
-    alert(`The user selected ${rate}`)
-  }
 }
