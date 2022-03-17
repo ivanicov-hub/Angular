0640dace10b947edfc47fca753eb2b3d71696119
Ivan Icov <ivan.icov@student.um.si>
2022-03-17 14:31:38 +0100

    Reacting to changes in the form

--- src/app/movies/movie-filter/movie-filter.component.ts
+++ src/app/movies/movie-filter/movie-filter.component.ts
@@ -14,6 +14,14 @@ export class MovieFilterComponent implements OnInit {
 
   genres = [{ id: 1, name: 'Drama' }, { id: 2, name: 'Comedy' }];
 
+  movies = [
+    { title: 'Spider-Man', poster: 'https://www.sonypictures.com/sites/default/files/styles/max_560x840/public/title-key-art/Spider-Man-FarFromHome-rating.jpg?itok=CGe-MMMn' },
+    { title: 'Moana', poster: 'https://lumiere-a.akamaihd.net/v1/images/p_moana_20530_214883e3.jpeg' },
+    { title: 'Inception', poster: 'https://flxt.tmsimg.com/assets/p7825626_p_v10_af.jpg' }
+  ];
+
+  originalMovies = this.movies;
+
   ngOnInit(): void {
     this.form = this.formBuilder.group({
       title: '',
@@ -21,10 +29,22 @@ export class MovieFilterComponent implements OnInit {
       upcomingReleases: false,
       inTheaters: false
     });
+
+    this.form.valueChanges.subscribe(values => {
+      this.movies = this.originalMovies;
+      this.filterMovies(values);
+    });
+
+  }
+
+  filterMovies(values: any) {
+    if (values.title) {
+      this.movies = this.movies.filter(movie => movie.title.indexOf(values.title) !== -1);
+    }
   }
 
   clearForm() {
-
+    this.form.reset();
   }
 
 }
