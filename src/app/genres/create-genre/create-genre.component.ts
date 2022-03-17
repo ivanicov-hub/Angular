af79f955095c63347943bf3cca8ec7c4026875b2
Ivan Icov <ivan.icov@student.um.si>
2022-03-17 11:26:30 +0100

    Added form validation rules for min length and empty field

--- src/app/genres/create-genre/create-genre.component.ts
+++ src/app/genres/create-genre/create-genre.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 
 @Component({
@@ -15,7 +15,9 @@ export class CreateGenreComponent implements OnInit {
 
   ngOnInit(): void {
     this.form = this.formBuilder.group({
-      name: ''
+      name: ['', {
+        validators: [Validators.required, Validators.minLength(3)]
+      }]
     });
   }
 
@@ -23,5 +25,18 @@ export class CreateGenreComponent implements OnInit {
     // ...save the genre
     this.router.navigate(['/genres']);
 
+  }
+
+  getErrorMessageFieldName() {
+    const field = this.form.get('name');
+    if (field.hasError('required')) {
+      return 'The name field is required';
+    }
+
+    if (field.hasError('minlength')) {
+      return 'The minimum length is 3';
+    }
+
+    return '';
   }
 }
