aaba92f484bb3cf0e1e5c3832a8e56008348ec0a
Ivan Icov <ivan.icov@student.um.si>
2022-03-17 11:59:30 +0100

    Sharing a form between multiple components

--- src/app/genres/create-genre/create-genre.component.ts
+++ src/app/genres/create-genre/create-genre.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { firstLetterUppercase } from 'src/app/validators/firstLetterUppercase';
+import { genreCreationDTO } from '../genres.model';
 
 @Component({
   selector: 'app-create-genre',
@@ -10,38 +11,14 @@ import { firstLetterUppercase } from 'src/app/validators/firstLetterUppercase';
 })
 export class CreateGenreComponent implements OnInit {
 
-  constructor(private router: Router, private formBuilder: FormBuilder) { }
-
-  form: FormGroup;
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
-    this.form = this.formBuilder.group({
-      name: ['', {
-        validators: [Validators.required, Validators.minLength(3), firstLetterUppercase()]
-      }]
-    });
   }
 
-  saveChanges() {
+  saveChanges(genreCreationDTO: genreCreationDTO) {
     // ...save the genre
+    console.log(genreCreationDTO);
     this.router.navigate(['/genres']);
-
-  }
-
-  getErrorMessageFieldName() {
-    const field = this.form.get('name');
-    if (field.hasError('required')) {
-      return 'The name field is required';
-    }
-
-    if (field.hasError('minlength')) {
-      return 'The minimum length is 3';
-    }
-
-    if (field.hasError('firstLetterUppercase')) {
-      return field.getError('firstLetterUppercase').message;
-    }
-
-    return '';
   }
 }
