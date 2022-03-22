b2a440c601ce4a3b2960d55333fd74110141b1f9
Ivan Icov <ivan.icov@student.um.si>
2022-03-22 14:45:39 +0100

    Created movie form

--- src/app/movies/edit-movie/edit-movie.component.ts
+++ src/app/movies/edit-movie/edit-movie.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { movieCreationDTO, movieDTO } from '../movies.models';
 
 @Component({
   selector: 'app-edit-movie',
@@ -10,10 +11,17 @@ export class EditMovieComponent implements OnInit {
 
   constructor(private activatedRoute: ActivatedRoute) { }
 
+  model: movieDTO = {
+    title: 'Spider-Man', inTheaters: true, summary: 'Whatever',
+    releaseDate: new Date(), trailer: 'SDFSDF', poster: 'https://ichef.bbci.co.uk/images/ic/640x360/p0b6kkh5.jpg'
+  }
   ngOnInit(): void {
     this.activatedRoute.params.subscribe(params => {
 
     })
   }
 
+  saveChanges(movieCreationDTO: movieCreationDTO) {
+
+  }
 }
