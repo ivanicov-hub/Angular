9c6f204617cb02527f1b727a0842fd0c093b5519
Ivan Icov <ivan.icov@student.um.si>
2022-03-23 23:45:58 +0100

    Custom validation rules for property Name added

--- MoviesAPI/Entities/Genre.cs
+++ MoviesAPI/Entities/Genre.cs
@@ -1,12 +1,15 @@
+using MoviesAPI.Validations;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
 namespace MoviesAPI.Entities
 {
-  public class Genre
+  public class Genre: IValidatableObject
   {
     public int Id { get; set; }
     [Required(ErrorMessage ="The field with name {0} is required")]
     [StringLength(10)]
+    //[FirstLetterUppercase]
     public string Name { get; set; }
 
     [Range(18, 120)]
@@ -15,5 +18,18 @@ namespace MoviesAPI.Entities
     public string CreditCard { get; set; }
     [Url]
     public string Url { get; set; }
+
+    public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+    {
+      if(!string.IsNullOrEmpty(Name))
+      {
+        var firstLetter = Name[0].ToString();
+
+        if(firstLetter != firstLetter.ToUpper())
+        {
+          yield return new ValidationResult("First letter should be uppercase", new string[] { nameof(Name) });
+        }
+      }
+    }
   }
 }
