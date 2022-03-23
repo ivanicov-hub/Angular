81b6d237e8c7925e82ee73db6c061fbb590617bd
Ivan Icov <ivan.icov@student.um.si>
2022-03-23 10:29:58 +0100

    Router rules basics

--- MoviesAPI/Controllers/GenresController.cs
+++ MoviesAPI/Controllers/GenresController.cs
@@ -18,14 +18,16 @@ namespace MoviesAPI.Controllers
       this.repository = repository;
     }
 
-    [HttpGet]
+    [HttpGet] //api/genres
+    [HttpGet("list")]
+    [HttpGet("/allgenres")]
     public List<Genre> Get()
     {
       return repository.GetAllGenres();
     }
 
-    [HttpGet]
-    public Genre Get(int Id)
+    [HttpGet("{Id:int}/{param2=felipe}")] //api/genres/example
+    public Genre Get(int Id, string param2)
     {
       var genre = repository.GetGenreById(Id);
 
